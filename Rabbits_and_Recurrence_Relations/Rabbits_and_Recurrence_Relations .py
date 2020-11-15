0452d81355fa4fab273f233bbe2f1015ed8488e4
thayanavt <thayana.vt@gmail.com>
2020-11-14 23:46:05 -0300

    Update Rabbits_and_Recurrence_Relations .py

--- Rabbits_and_Recurrence_Relations/Rabbits_and_Recurrence_Relations .py
+++ Rabbits_and_Recurrence_Relations/Rabbits_and_Recurrence_Relations .py
@@ -12,10 +12,3 @@ while i<=(n-3):
     i = i + 1
 
 print(geracoes)
-
-
-
-
-#Ex: No quinto mês, o total de coelhos será a multi de coelhos velhos (3 mes)
-#pelo tanto de filhos que eles podem ter + 3 (eles mesmos) + a diferença do 3 mes
-#para o quarto mes que são os coelhos bbs que ainda não podem ter filhos
