c3202c602d8c26d2ab66c265536b2f3226f208a6
thayanavt <thayana.vt@gmail.com>
2022-04-03 10:04:42 -0300

    Update Computing_GC_Content_Biopython.py

--- Computing_GC_Content/Computing_GC_Content_Biopython.py
+++ Computing_GC_Content/Computing_GC_Content_Biopython.py
@@ -1,21 +1,15 @@
 from Bio import SeqIO
 
-ids_list, G_list, C_list, seqs_size_list, percent_list = [], [], [], [], []
-G, C = 0, 0
+max_total = 0
 
-with open("/home/thayana/Documentos/programas_python/ROSALIND/Computing_GC_Content/Computing_GC_Content.txt") as file_dna:
-    for sequencia in SeqIO.parse(file_dna, "fasta"):
-        G_list.append(str(sequencia.seq).count("G"))
-        C_list.append(str(sequencia.seq).count("C"))
-        ids_list.append(sequencia.id)
-        seqs_size_list.append(len(sequencia))
-    
-for G, C, seq_size in zip(G_list, C_list, seqs_size_list):
-    percent_list.append(((G+C)/seq_size) * 100)
+for record in SeqIO.parse("example.fasta", "fasta"):
+  G = str(record.seq).count("G")
+  C = str(record.seq).count("C")
+  total = ((G+C)/len(record.seq))*100
 
-max_percent_index = percent_list.index(max(percent_list))
+  if total >= max_total:
+    max_total = total
+    id = record.id
 
-print(G_list)
-print(percent_list)
-print(ids_list[max_percent_index])
-print(percent_list[max_percent_index])
+print(f">{id}")
+print(max_total)
